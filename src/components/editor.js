8a82e696cef19f13ab5126926766872914d7e43d
Pratyush Garg <pratyushhg@gmail.com>
2021-04-07 17:40:18 +0530

    added editor

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,7 +1,39 @@
 import React from "react";
 
-const Editor = () => {
-  return <div>Editor</div>;
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
+import { Controlled as ControlledEditor } from "react-codemirror2";
+
+const Editor = (props) => {
+  const { displayName, language, value, onChange } = props;
+
+  var handleChange = (editor, data, value) => {
+    onChange(value);
+  };
+
+  return (
+    <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>O/C</button>
+      </div>
+      <ControlledEditor
+        onBeforeChange={handleChange}
+        value={value}
+        className="code-mirror-wrapper"
+        options={{
+          lineWrapping: true,
+          mode: language,
+          lint: true,
+          lineNumbers: true,
+          theme: "material",
+        }}
+      />
+    </div>
+  );
 };
 
 export default Editor;
